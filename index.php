7374336c14733a9bccd2c43495888e9cfb0ab348
Kyle Middleton <kylemid321@gmail.com>
2019-04-20 19:19:03 -0600

    added form and connected to action.php

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     </head>
     <body>
         <?php echo '<p>Hello World</p>'; ?>
+        <form action="action.php" method="post">
+            <p>Your name: <input type="text" name="name" /></p>
+            <p>Your age: <input type="text" name="age" /></p>
+            <p><input type="submit" /></p>
+        </form>
         <?php phpinfo(); ?>
         <?php
             if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
@@ -14,6 +19,7 @@
                 echo 'You are using Chrome.';
             }
         ?>
+        
 
     </body>
 </html>
