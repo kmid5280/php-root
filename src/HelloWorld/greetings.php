fff0306f372fd39c73c28b6ed43e72f37b948bd8
Kyle Middleton <kylemid321@gmail.com>
2019-04-21 13:34:36 -0600

    initial commit

---
+++ src/HelloWorld/greetings.php
@@ -0,0 +1,14 @@
+<?php
+namespace HelloWorld;
+
+//use PHP_Timer;
+
+class Greetings {
+    public static function sayHelloWorld() {
+        //$timer = new PHP_Timer();
+        //$timer->start();
+        return "Hello world\n" /*. $timer->resourceUsage() . "\n"*/;
+    }
+}
+
+?>
---
+++ tests/test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use HelloWorld\Greetings;
+
+echo Greetings::sayHelloWorld();
+
+?>
