493edc98b815336328955d52affbeaa4c54067b8
Kyle Middleton <kylemid321@gmail.com>
2019-04-28 14:07:32 -0600

    setup env configuration

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <title>PHP Test</title>
     </head>
     <body>
+        
         <form action="action.php" method="post">
             <p>Your name: <input type="text" name="name" /></p>
             <p>Your age: <input type="text" name="age" /></p>
@@ -17,7 +18,26 @@
                 echo 'You are using Chrome.';
             }
         ?>
-        
 
+        <?php
+            require __DIR__ . '/vendor/autoload.php';
+            $dotenv = Dotenv\Dotenv::create(__DIR__);
+            $dotenv->load();
+            $servername = getenv('servername');
+            $username = getenv('username');
+            $password = getenv('password');
+            $myDB = getenv('myDB');
+
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+                }
+            catch(PDOException $e)
+                {
+                echo "Connection failed: " . $e->getMessage();
+                }
+        ?> 
     </body>
 </html>
