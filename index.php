d8342801cffd47ca8736436b84d2a9d8170ef3f7
Kyle Middleton <kylemid321@gmail.com>
2019-04-28 13:06:36 -0600

    remove unneeded code

--- index.php
+++ index.php
@@ -3,13 +3,11 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?>
         <form action="action.php" method="post">
             <p>Your name: <input type="text" name="name" /></p>
             <p>Your age: <input type="text" name="age" /></p>
             <p><input type="submit" /></p>
         </form>
-        <?php phpinfo(); ?>
         <?php
             if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
                 echo 'You are using MSIE.';
