0eea8283c77561c2a4e25ff4f09689abcb16f1fe
Kyle Middleton <kylemid321@gmail.com>
2019-05-02 18:10:40 -0600

    show table with existing guests from database

--- index.php
+++ index.php
@@ -41,6 +41,22 @@
         $password = getenv('password');
         $dbname = getenv('dbname');
 
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
+            $stmt->execute();
+
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+                echo $v;
+            }
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e>getMessage();
+        }
+        $conn = null;
+        echo "</table>";
 
         ?>
         <?php
